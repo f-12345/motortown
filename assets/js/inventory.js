68e7d20d5acc1ad7d6a3c609e2653edd8f3c32a1
f-12345 <lunasautoshop@gmail.com>
2026-05-13 11:37:49 -0500

    Update inventory.js

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -1,88 +1,433 @@
 const cars = [
+  // {
+  //   title: "2019 Nissan Armada SV 4WD",
+  //   miles: "22,278",
+  //   trans: "Automatic",
+  //   price: 18990,
+  //   tag: "4WD",
+  //   img: "",
+  //   photos: [],
+  //   vin: "JN8AY2NC8K9580189",
+  //   color: "Black",
+  //   engine: "5.6L V8",
+  //   drivetrain: "Four-Wheel Drive",
+  // },
   {
-    title: "2021 Mercedes-Benz C300",
-    miles: "32,410",
-    trans: "Automatic",
-    price: 31995,
-    tag: "Clean Title",
-    img: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?auto=format&fit=crop&w=1800&q=60",
+    title: "2019 Lamborghini Urus",
+    miles: "19,664",
+    trans: "8-Speed Automatic",
+    price: 174990,
+    tag: "Exotic",
+    img: "assets/images/lamborghini-urus-2019-01.jpg",
+    photos: [
+      "assets/images/lamborghini-urus-2019-01.jpg",
+      "assets/images/lamborghini-urus-2019-02.jpg",
+      "assets/images/lamborghini-urus-2019-03.jpg",
+      "assets/images/lamborghini-urus-2019-04.jpg",
+      "assets/images/lamborghini-urus-2019-05.jpg",
+      "assets/images/lamborghini-urus-2019-06.jpg",
+      "assets/images/lamborghini-urus-2019-07.jpg",
+      "assets/images/lamborghini-urus-2019-08.jpg",
+      "assets/images/lamborghini-urus-2019-09.jpg",
+      "assets/images/lamborghini-urus-2019-10.jpg",
+      "assets/images/lamborghini-urus-2019-11.jpg",
+      "assets/images/lamborghini-urus-2019-12.jpg",
+    ],
+    vin: "ZPBUA1ZL1KLA01041",
+    color: "Gray",
+    engine: "4.0L V8 Turbo (641 hp)",
+    drivetrain: "All-Wheel Drive",
   },
   {
-    title: "2020 Toyota Camry SE",
-    miles: "41,220",
-    trans: "Automatic",
-    price: 22990,
-    tag: "One Owner",
-    img: "https://images.unsplash.com/photo-1542362567-b07e54358753?auto=format&fit=crop&w=1800&q=60",
-  },
-  {
-    title: "2019 Ford F-150 XLT 4x4",
-    miles: "75,800",
-    trans: "Automatic",
-    price: 28800,
-    tag: "4x4",
-    img: "https://images.unsplash.com/photo-1517524008697-84bbe3c3fd98?auto=format&fit=crop&w=1800&q=60",
-  },
-  {
-    title: "2018 BMW 330i",
-    miles: "62,110",
-    trans: "Automatic",
-    price: 19900,
+    title: "2018 BMW X6 sDrive35i",
+    miles: "55,700",
+    trans: "8-Speed Automatic",
+    price: 19990,
     tag: "Sport",
-    img: "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?auto=format&fit=crop&w=1800&q=60",
+    img: "assets/images/bmw-x6-2018-01.jpg",
+    photos: [
+      "assets/images/bmw-x6-2018-01.jpg",
+      "assets/images/bmw-x6-2018-02.jpg",
+      "assets/images/bmw-x6-2018-03.jpg",
+      "assets/images/bmw-x6-2018-04.jpg",
+      "assets/images/bmw-x6-2018-05.jpg",
+      "assets/images/bmw-x6-2018-06.jpg",
+      "assets/images/bmw-x6-2018-07.jpg",
+      "assets/images/bmw-x6-2018-08.jpg",
+      "assets/images/bmw-x6-2018-09.jpg",
+      "assets/images/bmw-x6-2018-10.jpg",
+      "assets/images/bmw-x6-2018-11.jpg",
+      "assets/images/bmw-x6-2018-12.jpg",
+      "assets/images/bmw-x6-2018-13.jpg",
+    ],
+    vin: "5UXKU0C57J0G80499",
+    color: "White",
+    engine: "3.0L I6 (300 hp)",
+    drivetrain: "Rear-Wheel Drive",
   },
   {
-    title: "2022 Honda Civic Touring",
-    miles: "18,600",
-    trans: "Automatic",
-    price: 25995,
-    tag: "Low Miles",
-    img: "https://images.unsplash.com/photo-1503376780353-7e6692767b70?auto=format&fit=crop&w=1800&q=60",
+    title: "2023 Chevrolet Malibu LS",
+    miles: "46,560",
+    trans: "CVT",
+    price: 16990,
+    tag: "Clean",
+    img: "assets/images/chevy-malibu-2023-01.jpg",
+    photos: [
+      "assets/images/chevy-malibu-2023-01.jpg",
+      "assets/images/chevy-malibu-2023-02.jpg",
+      "assets/images/chevy-malibu-2023-03.jpg",
+      "assets/images/chevy-malibu-2023-04.jpg",
+      "assets/images/chevy-malibu-2023-05.jpg",
+      "assets/images/chevy-malibu-2023-06.jpg",
+      "assets/images/chevy-malibu-2023-07.jpg",
+      "assets/images/chevy-malibu-2023-08.jpg",
+      "assets/images/chevy-malibu-2023-09.jpg",
+      "assets/images/chevy-malibu-2023-10.jpg",
+      "assets/images/chevy-malibu-2023-11.jpg",
+      "assets/images/chevy-malibu-2023-12.jpg",
+      "assets/images/chevy-malibu-2023-13.jpg",
+    ],
+    vin: "1G1ZC5STXPF116714",
+    color: "White",
+    engine: "1.5L I4 (160 hp)",
+    drivetrain: "Front-Wheel Drive",
   },
   {
-    title: "2017 Jeep Grand Cherokee",
-    miles: "89,200",
+    title: "2020 Dodge Charger GT",
+    miles: "44,571",
     trans: "Automatic",
-    price: 17950,
+    price: 12990,
+    tag: "RWD",
+    img: "assets/images/dodge-charger-2020-01.jpg",
+    photos: [
+      "assets/images/dodge-charger-2020-01.jpg",
+      "assets/images/dodge-charger-2020-02.jpg",
+      "assets/images/dodge-charger-2020-03.jpg",
+      "assets/images/dodge-charger-2020-04.jpg",
+      "assets/images/dodge-charger-2020-05.jpg",
+      "assets/images/dodge-charger-2020-06.jpg",
+      "assets/images/dodge-charger-2020-07.jpg",
+      "assets/images/dodge-charger-2020-08.jpg",
+      "assets/images/dodge-charger-2020-09.jpg",
+      "assets/images/dodge-charger-2020-10.jpg",
+      "assets/images/dodge-charger-2020-11.jpg",
+      "assets/images/dodge-charger-2020-12.jpg",
+    ],
+    vin: "2C3CDXHG5LH195495",
+    color: "Red",
+    engine: "3.6L V6 (300 hp)",
+    drivetrain: "Rear-Wheel Drive",
+  },
+  {
+    title: "2017 Maserati Ghibli S Q4",
+    miles: "60,890",
+    trans: "Automatic",
+    price: 13990,
+    tag: "AWD",
+    img: "assets/images/maserati-ghibli-2017-01.jpg",
+    photos: [
+      "assets/images/maserati-ghibli-2017-01.jpg",
+      "assets/images/maserati-ghibli-2017-02.jpg",
+      "assets/images/maserati-ghibli-2017-03.jpg",
+      "assets/images/maserati-ghibli-2017-04.jpg",
+      "assets/images/maserati-ghibli-2017-05.jpg",
+      "assets/images/maserati-ghibli-2017-06.jpg",
+      "assets/images/maserati-ghibli-2017-07.jpg",
+      "assets/images/maserati-ghibli-2017-08.jpg",
+      "assets/images/maserati-ghibli-2017-09.jpg",
+      "assets/images/maserati-ghibli-2017-10.jpg",
+      "assets/images/maserati-ghibli-2017-11.jpg",
+      "assets/images/maserati-ghibli-2017-12.jpg",
+    ],
+    vin: "ZAM57RTA7H1213653",
+    color: "Gray",
+    engine: "3.0L V6 (404 hp)",
+    drivetrain: "All-Wheel Drive",
+  },
+  {
+    title: "2014 RAM 1500 Laramie Limited",
+    miles: "147,132",
+    trans: "8-Speed Automatic",
+    price: 13990,
+    tag: "Truck",
+    img: "assets/images/ram-1500-2014-01.jpg",
+    photos: [
+      "assets/images/ram-1500-2014-01.jpg",
+      "assets/images/ram-1500-2014-02.jpg",
+      "assets/images/ram-1500-2014-03.jpg",
+      "assets/images/ram-1500-2014-04.jpg",
+      "assets/images/ram-1500-2014-05.jpg",
+      "assets/images/ram-1500-2014-06.jpg",
+      "assets/images/ram-1500-2014-07.jpg",
+      "assets/images/ram-1500-2014-08.jpg",
+      "assets/images/ram-1500-2014-09.jpg",
+      "assets/images/ram-1500-2014-10.jpg",
+      "assets/images/ram-1500-2014-11.jpg",
+      "assets/images/ram-1500-2014-12.jpg",
+      "assets/images/ram-1500-2014-13.jpg",
+    ],
+    vin: "1C6RR6PT3ES271850",
+    color: "Black",
+    engine: "5.7L V8 (395 hp)",
+    drivetrain: "4x2",
+  },
+  {
+    title: "2013 Mercedes-Benz S550",
+    miles: "98,403",
+    trans: "Automatic",
+    price: 11990,
+    tag: "Luxury",
+    img: "assets/images/mercedes-s550-2013-01.jpg",
+    photos: [
+      "assets/images/mercedes-s550-2013-01.jpg",
+      "assets/images/mercedes-s550-2013-02.jpg",
+      "assets/images/mercedes-s550-2013-03.jpg",
+      "assets/images/mercedes-s550-2013-04.jpg",
+      "assets/images/mercedes-s550-2013-05.jpg",
+      "assets/images/mercedes-s550-2013-06.jpg",
+      "assets/images/mercedes-s550-2013-07.jpg",
+      "assets/images/mercedes-s550-2013-08.jpg",
+      "assets/images/mercedes-s550-2013-09.jpg",
+      "assets/images/mercedes-s550-2013-10.jpg",
+      "assets/images/mercedes-s550-2013-11.jpg",
+      "assets/images/mercedes-s550-2013-12.jpg",
+      "assets/images/mercedes-s550-2013-13.jpg",
+    ],
+    vin: "WDDNG7DB3DA512167",
+    color: "Black",
+    engine: "4.7L V8 (429 hp)",
+    drivetrain: "Rear-Wheel Drive",
+  },
+  {
+    title: "2018 Nissan Rogue SV",
+    miles: "67,773",
+    trans: "CVT",
+    price: 11990,
     tag: "SUV",
-    img: "https://images.unsplash.com/photo-1519641471654-76ce0107ad1b?auto=format&fit=crop&w=1800&q=60",
-  }
+    img: "assets/images/nissan-rogue-2018-01.jpg",
+    photos: [
+      "assets/images/nissan-rogue-2018-01.jpg",
+      "assets/images/nissan-rogue-2018-02.jpg",
+      "assets/images/nissan-rogue-2018-03.jpg",
+      "assets/images/nissan-rogue-2018-04.jpg",
+      "assets/images/nissan-rogue-2018-05.jpg",
+      "assets/images/nissan-rogue-2018-06.jpg",
+      "assets/images/nissan-rogue-2018-07.jpg",
+      "assets/images/nissan-rogue-2018-08.jpg",
+      "assets/images/nissan-rogue-2018-09.jpg",
+      "assets/images/nissan-rogue-2018-10.jpg",
+      "assets/images/nissan-rogue-2018-11.jpg",
+      "assets/images/nissan-rogue-2018-12.jpg",
+      "assets/images/nissan-rogue-2018-13.jpg",
+    ],
+    vin: "5N1AT2MT9J758572",
+    color: "White",
+    engine: "2.5L I4 (170 hp)",
+    drivetrain: "Front-Wheel Drive",
+  },
+  {
+    title: "2017 Audi A6 2.0T Premium",
+    miles: "133,057",
+    trans: "Automatic",
+    price: 7990,
+    tag: "Value",
+    img: "assets/images/audi-a6-2017-01.jpg",
+    photos: [
+      "assets/images/audi-a6-2017-01.jpg",
+      "assets/images/audi-a6-2017-02.jpg",
+      "assets/images/audi-a6-2017-03.jpg",
+      "assets/images/audi-a6-2017-04.jpg",
+      "assets/images/audi-a6-2017-05.jpg",
+      "assets/images/audi-a6-2017-06.jpg",
+      "assets/images/audi-a6-2017-07.jpg",
+      "assets/images/audi-a6-2017-08.jpg",
+      "assets/images/audi-a6-2017-09.jpg",
+      "assets/images/audi-a6-2017-10.jpg",
+      "assets/images/audi-a6-2017-11.jpg",
+      "assets/images/audi-a6-2017-12.jpg",
+      "assets/images/audi-a6-2017-13.jpg",
+    ],
+    vin: "WAUC8AFC9HN092086",
+    color: "Gray",
+    engine: "2.0L I4",
+    drivetrain: "Front-Wheel Drive",
+  },
+  {
+    title: "2012 BMW 750i",
+    miles: "132,198",
+    trans: "6-Speed Automatic",
+    price: 8990,
+    tag: "Luxury",
+    img: "assets/images/bmw-750i-2012-01.jpg",
+    photos: [
+      "assets/images/bmw-750i-2012-01.jpg",
+      "assets/images/bmw-750i-2012-02.jpg",
+      "assets/images/bmw-750i-2012-03.jpg",
+      "assets/images/bmw-750i-2012-04.jpg",
+      "assets/images/bmw-750i-2012-05.jpg",
+      "assets/images/bmw-750i-2012-06.jpg",
+      "assets/images/bmw-750i-2012-07.jpg",
+      "assets/images/bmw-750i-2012-08.jpg",
+      "assets/images/bmw-750i-2012-09.jpg",
+      "assets/images/bmw-750i-2012-10.jpg",
+      "assets/images/bmw-750i-2012-11.jpg",
+      "assets/images/bmw-750i-2012-12.jpg",
+      "assets/images/bmw-750i-2012-13.jpg",
+    ],
+    vin: "WBAKA8C58CDX01178",
+    color: "Black",
+    engine: "4.4L V8 (400 hp)",
+    drivetrain: "Rear-Wheel Drive",
+  },
+  {
+    title: "2019 Nissan Sentra",
+    miles: "114,490",
+    trans: "Automatic",
+    price: null,
+    tag: "Sedan",
+    img: "assets/images/nissan-sentra-2019-01.jpg",
+    photos: [
+      "assets/images/nissan-sentra-2019-01.jpg",
+      "assets/images/nissan-sentra-2019-02.jpg",
+      "assets/images/nissan-sentra-2019-03.jpg",
+      "assets/images/nissan-sentra-2019-04.jpg",
+      "assets/images/nissan-sentra-2019-05.jpg",
+      "assets/images/nissan-sentra-2019-06.jpg",
+      "assets/images/nissan-sentra-2019-07.jpg",
+      "assets/images/nissan-sentra-2019-08.jpg",
+      "assets/images/nissan-sentra-2019-09.jpg",
+      "assets/images/nissan-sentra-2019-10.jpg",
+      "assets/images/nissan-sentra-2019-11.jpg",
+      "assets/images/nissan-sentra-2019-12.jpg",
+      "assets/images/nissan-sentra-2019-13.jpg",
+      "assets/images/nissan-sentra-2019-14.jpg",
+    ],
+    vin: "3N1AB7AP8KY342519",
+    color: "White",
+    engine: "1.8L I4",
+    drivetrain: "Front-Wheel Drive",
+  },
 ];
 
-function money(n){
+function money(n) {
+  if (n === null || n === undefined) return "Call for Price";
   return n.toLocaleString("en-US", { style: "currency", currency: "USD", maximumFractionDigits: 0 });
 }
 
-function render(list){
+// Lightbox state
+let lbCarIdx = -1;
+let lbPhotoIdx = 0;
+
+function openLightbox(carIdx, photoIdx) {
+  lbCarIdx = carIdx;
+  lbPhotoIdx = photoIdx || 0;
+  updateLightbox();
+  const lb = document.getElementById("lightbox");
+  lb.classList.add("open");
+  lb.setAttribute("aria-hidden", "false");
+  document.body.style.overflow = "hidden";
+}
+
+function closeLightbox() {
+  const lb = document.getElementById("lightbox");
+  lb.classList.remove("open");
+  lb.setAttribute("aria-hidden", "true");
+  document.body.style.overflow = "";
+}
+
+function updateLightbox() {
+  const car = cars[lbCarIdx];
+  if (!car) return;
+  const photos = car.photos && car.photos.length ? car.photos : [car.img];
+  const imgEl = document.getElementById("lbImg");
+  const counterEl = document.getElementById("lbCounter");
+  const titleEl = document.getElementById("lbTitle");
+  const prevBtn = document.getElementById("lbPrev");
+  const nextBtn = document.getElementById("lbNext");
+
+  imgEl.src = photos[lbPhotoIdx];
+  imgEl.alt = car.title;
+  if (titleEl) titleEl.textContent = car.title;
+  const multi = photos.length > 1;
+  if (counterEl) counterEl.textContent = multi ? `${lbPhotoIdx + 1} / ${photos.length}` : "";
+  if (prevBtn) prevBtn.style.display = multi ? "flex" : "none";
+  if (nextBtn) nextBtn.style.display = multi ? "flex" : "none";
+}
+
+function lbNext() {
+  const car = cars[lbCarIdx] || {};
+  const photos = (car.photos && car.photos.length) ? car.photos : [car.img];
+  if (!photos.length) return;
+  lbPhotoIdx = (lbPhotoIdx + 1) % photos.length;
+  updateLightbox();
+}
+
+function lbPrev() {
+  const car = cars[lbCarIdx] || {};
+  const photos = (car.photos && car.photos.length) ? car.photos : [car.img];
+  if (!photos.length) return;
+  lbPhotoIdx = (lbPhotoIdx - 1 + photos.length) % photos.length;
+  updateLightbox();
+}
+
+function render(list) {
   const grid = document.querySelector("#invGrid");
   if (!grid) return;
 
-  grid.innerHTML = list.map((c) => `
-    <div class="inv-card">
-      <img class="inv-img" src="${c.img}" alt="${c.title}">
-      <div class="inv-body">
-        <h3 class="inv-title">${c.title}</h3>
-        <div class="inv-meta">${c.miles} miles • ${c.trans}</div>
-        <div class="inv-price">
-          <div style="font-weight:800; font-size:18px;">${money(c.price)}</div>
-          <div class="badge">${c.tag}</div>
-        </div>
-        <div class="btn-row" style="margin-top:6px;">
-          <a class="btn primary" href="contact.html">Check Availability</a>
-          <a class="btn" href="financing.html#prequal">Get Pre-Qualified</a>
+  if (list.length === 0) {
+    grid.innerHTML = `<div class="card" style="grid-column:1/-1;"><h3>No vehicles match your search.</h3><p>Try adjusting your filters.</p></div>`;
+    return;
+  }
+
+  grid.innerHTML = list.map((c) => {
+    const carIdx = cars.indexOf(c);
+    const photos = (c.photos && c.photos.length) ? c.photos : [c.img];
+    const multi = photos.length > 1;
+
+    const details = [
+      c.color      ? `<div class="inv-line"><span class="inv-k">Color</span><span class="inv-v">${c.color}</span></div>` : "",
+      c.engine     ? `<div class="inv-line"><span class="inv-k">Engine</span><span class="inv-v">${c.engine}</span></div>` : "",
+      c.drivetrain ? `<div class="inv-line"><span class="inv-k">Drivetrain</span><span class="inv-v">${c.drivetrain}</span></div>` : "",
+      c.vin        ? `<div class="inv-line"><span class="inv-k">VIN</span><span class="inv-v inv-vin">${c.vin}</span></div>` : "",
+    ].filter(Boolean).join("");
+
+    const imgSection = c.img ? `
+        <div class="inv-img-wrap" onclick="openLightbox(${carIdx}, 0)">
+          <img class="inv-img" src="${c.img}" alt="${c.title}">
+          <div class="img-hover-overlay"><span>View Photos</span></div>
+          ${multi ? `<div class="photo-count-badge">${photos.length} photos</div>` : ""}
+        </div>` : "";
+
+    return `
+      <div class="inv-card">
+        ${imgSection}
+        <div class="inv-body">
+          <h3 class="inv-title">${c.title}</h3>
+          <div class="inv-meta">${c.miles} miles &bull; ${c.trans}</div>
+          <div class="inv-price">
+            <div style="font-weight:800; font-size:18px;">${money(c.price)}</div>
+            <div class="badge">${c.tag}</div>
+          </div>
+          <div class="inv-details">${details}</div>
+          <div class="btn-row" style="margin-top:10px;">
+            <a class="btn primary" href="contact.html">Check Availability</a>
+            <a class="btn" href="get-prequalified.html">Get Pre-Qualified</a>
+          </div>
         </div>
       </div>
-    </div>
-  `).join("");
+    `;
+  }).join("");
 }
 
-function applyFilters(){
+function applyFilters() {
   const q = (document.querySelector("#q")?.value || "").toLowerCase();
   const max = Number(document.querySelector("#maxPrice")?.value || 999999);
 
   const filtered = cars.filter((c) => {
-    const matchesText = c.title.toLowerCase().includes(q);
-    const matchesPrice = c.price <= max;
+    const matchesText = c.title.toLowerCase().includes(q) ||
+                        (c.color || "").toLowerCase().includes(q) ||
+                        (c.tag || "").toLowerCase().includes(q);
+    const matchesPrice = c.price === null || c.price <= max;
     return matchesText && matchesPrice;
   });
 
@@ -110,9 +455,34 @@ document.addEventListener("DOMContentLoaded", () => {
   if (clearBtn) {
     clearBtn.addEventListener("click", () => {
       if (q) q.value = "";
-      if (max) max.value = "50000";
-      if (maxLabel) maxLabel.textContent = money(50000);
+      if (max) max.value = "200000";
+      if (maxLabel) maxLabel.textContent = money(200000);
       render(cars);
+    });
+  }
+
+  // Lightbox controls
+  document.getElementById("lbClose")?.addEventListener("click", closeLightbox);
+  document.getElementById("lbNext")?.addEventListener("click", lbNext);
+  document.getElementById("lbPrev")?.addEventListener("click", lbPrev);
+  document.getElementById("lbOverlay")?.addEventListener("click", closeLightbox);
+
+  // Keyboard navigation
+  document.addEventListener("keydown", (e) => {
+    if (!document.getElementById("lightbox")?.classList.contains("open")) return;
+    if (e.key === "Escape") closeLightbox();
+    if (e.key === "ArrowRight") lbNext();
+    if (e.key === "ArrowLeft") lbPrev();
+  });
+
+  // Touch swipe
+  let touchStartX = 0;
+  const lbImg = document.getElementById("lbImg");
+  if (lbImg) {
+    lbImg.addEventListener("touchstart", (e) => { touchStartX = e.touches[0].clientX; }, { passive: true });
+    lbImg.addEventListener("touchend", (e) => {
+      const dx = e.changedTouches[0].clientX - touchStartX;
+      if (Math.abs(dx) > 50) dx < 0 ? lbNext() : lbPrev();
     });
   }
 });
